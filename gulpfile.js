a517e90528fcca83882591851ba5442bb554e26d
BrianCraddock <BrianLCraddock@gmail.com>
2016-11-20 22:23:58 -0500

    lots of updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+// the default task
+gulp.task('sass-variables', function() {
+  return gulp.src(['src/styles/sprak/sprak-variables.scss'])
+       .pipe(concat('sprak-variables.scss'))
+       .pipe(gulp.dest('./dist/export'));
+});
 
 gulp.task('sass', function(){
   return gulp.src('src/styles/sprak/sprak.scss')
